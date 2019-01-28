d624066af8dd947b07c6a2fbe5cdc50f7e9851f4
Edge Fund Analytics <edgefundanalytics@gmail.com>
2019-01-27 23:23:48 -0600

    Fixing errors in the code.

--- LibraryApiNew/Models/BelongsToUser.cs
+++ LibraryApiNew/Models/BelongsToUser.cs
@@ -7,7 +7,9 @@ namespace LibraryApiNew.Models
     {
           public int UserId { get; set; }        
           public User User { get; set; }
-
-          [Attr("username"), NotMapped] public string Username { get => User?.Username; }
+        
+          [Attr("username"), NotMapped] public string Username { get => User?.Username; set {} }
+          // Video 51 issue fixed 1-20-2019. UserName property must have a dedicated setter.
+          // Username { get => User?.Username; } should be Username { get => User?.Username; set {} }
     }
 }
